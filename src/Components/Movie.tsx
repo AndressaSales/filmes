310cfa74f383ef162f306ea7c2f998748ec1d12b
AndressaSales <andressaccs2001@gmail.com>
2024-11-17 18:27:52 -0300

    atualizando o movie

--- src/Components/Movie.tsx
+++ src/Components/Movie.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
 
-
 export default function Movie(){
     const [movies, setMovies] = useState<any[]>([])
 
@@ -14,7 +13,7 @@ export default function Movie(){
                 language: 'pt-BR'
             }
         }).then(response => {
-            console.log(response)
+            console.log(response.data.results)
             setMovies(response.data.results)
         })
     }
@@ -22,8 +21,24 @@ export default function Movie(){
     useEffect(()=> {
         getMovies()
     },[])
+
+
     return(
         <>
+            <div className=" mx-auto max-w-6xl mb-16 mt-10 grid grid-cols-1 md:grid-cols-3 cursor-pointer gap-7 ">
+                {
+                    movies.map((movie) => {
+                      return(
+                        <div key={movie.id}>
+                            <div className="flex items-center justify-between">
+                                <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt=""  />
+                                <h1>{movie.title}</h1>
+                            </div>
+                        </div>
+                      )
+                    })
+                }
+            </div>
         </>
     )
 }
--- src/routes/Movies.tsx
+++ src/routes/Movies.tsx
@@ -1,7 +1,12 @@
+import Movie from "../Components/Movie";
+
 export default function Movies(){
     return(
         <>
-        page movie
+            <div>
+                <h1 className="text-[#fff] p-5 font-bold text-[2.5rem]">Filmes</h1>
+                <Movie />
+            </div>
         </>
     )
 }
